faef5a80dff0c83b2c5d505c1d650a197f158dfc
DeathWish99 <44440688+DeathWish99@users.noreply.github.com>
2019-08-17 21:14:14 +0700

    Level Timer v1

---
+++ Project Cruise/Assets/LevelTimer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTimer : MonoBehaviour
+{
+    public bool gameState;
+    public float timer;
+    public GameObject endPanel;
+    public Text timerText;
+    public Text finalTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0f;
+        gameState = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IncrementTimer();
+        if(!gameState)
+        {
+            EndGame();
+        }
+    }
+
+    public void IncrementTimer()
+    {
+        timer += 1 * Time.deltaTime;
+        timerText.text = "Timer: " + Mathf.Round(timer).ToString();
+    }
+
+    public void SetState()
+    {
+        gameState = false;
+    }
+
+    public void EndGame()
+    {
+        Time.timeScale = 0;
+        endPanel.SetActive(true);
+        finalTime.text = "Final Time: " + Mathf.Round(timer).ToString();
+    }
+}
