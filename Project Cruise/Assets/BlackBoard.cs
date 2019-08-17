7cffc77e9c4dacb847fbf0fa46fbe6e45a534b5c
Phobez <36761666+Phobez@users.noreply.github.com>
2019-08-17 10:10:51 +0700

    Pushed existing scripts
    - BlackBoard.cs
    - LevelManager.cs
    - PlayerController.cs

---
+++ Project Cruise/Assets/BlackBoard.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A script to keep all the important references in one place
+/// </summary>
+public class Blackboard : MonoBehaviour
+{
+    private Joystick joystick;
+    private LevelManager levelManager;
+
+    public static Blackboard instance;
+
+    private void Awake()
+    {
+        instance = new Blackboard();
+    }
+
+    /////// PROPERTIES ///////
+    public Joystick Joystick
+    {
+        get
+        {
+            return joystick;
+        }
+        set
+        {
+            if(joystick == null)
+                joystick = value;
+        }
+    }
+
+    /////// PROPERTIES ///////
+    public LevelManager LevelManager
+    {
+        get
+        {
+            return levelManager;
+        }
+        set
+        {
+            if (levelManager == null)
+                levelManager = value;
+        }
+    }
+    
+}
