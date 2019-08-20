a27beb41293527c1f8c90271b1c532eb733c5337
Phobez <36761666+Phobez@users.noreply.github.com>
2019-08-20 13:06:29 +0700

    Created Scene Transition utility script
    Handles scene transitions by click or key down.

---
+++ Project Cruise/Assets/Scripts/Utilities/Editor/SceneTransitionEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SceneTransition))]
+[CanEditMultipleObjects]
+public class SceneTransitionEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        SceneTransition sceneTransition = target as SceneTransition;
+
+        sceneTransition.transitionType = (SceneTransition.TransitionType)EditorGUILayout.EnumPopup("Transition Type", sceneTransition.transitionType);
+
+        if (sceneTransition.transitionType == SceneTransition.TransitionType.KeyDown)
+        {
+            sceneTransition.keyToPress = (KeyCode) EditorGUILayout.EnumPopup("Key To Press", sceneTransition.keyToPress);
+            sceneTransition.targetScene = EditorGUILayout.TextField("Target Scene Name", sceneTransition.targetScene);
+        }
+    }
+}
---
+++ Project Cruise/Assets/Scripts/Utilities/SceneTransition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public enum TransitionType { Click, KeyDown };
+
+    public TransitionType transitionType = TransitionType.Click;
+    public KeyCode keyToPress = KeyCode.Escape;
+    public string targetScene = "";
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (transitionType == TransitionType.KeyDown)
+        {
+            StartCoroutine(LoadSceneOnKeyDown());
+        }
+    }
+
+    public void LoadSceneByName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private IEnumerator LoadSceneOnKeyDown()
+    {
+        while (!Input.GetKeyDown(keyToPress))
+        {
+            yield return null;
+        }
+
+        SceneManager.LoadScene(targetScene);
+    }
+}
