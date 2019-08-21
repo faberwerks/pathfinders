d7b39a0b7256086a727f015a3ba1536e1df5e6cf
Crouston <yosuam34@gmail.com>
2019-08-21 23:26:43 +0700

    Level manager implemented
    -Added some codes to levelManager Scripts

--- Project Cruise/Assets/Scripts/LevelManager.cs
+++ Project Cruise/Assets/Scripts/LevelManager.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Blackboard.instance.LevelManager = this;
-    }
+    private int treasureCollected;
 
-    // Update is called once per frame
-    void Update()
+    private bool hasRelic,
+                 relicCollected;
+
+    private float levelTimer;
+
+    //Awake is called before Start
+    private void Awake()
     {
         
     }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Blackboard.instance.LevelManager = this;
+        treasureCollected = 0;
+        relicCollected = false;
+    }
+
+    /////// PROPERTIES ///////
+    public int TreasureCollected
+    {
+        get
+        {
+            return treasureCollected;
+        }
+        set
+        {
+            treasureCollected += value;
+        }
+    }
+
+    /////// PROPERTIES ///////
+    public bool RelicCollected
+    {
+        get
+        {
+            return relicCollected;
+        }
+        set
+        {
+            relicCollected = value;
+        }
+    }
+
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
 }
