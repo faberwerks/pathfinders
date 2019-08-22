487a435370755b560da626ec08c1b82672a068a8
Phobez <36761666+Phobez@users.noreply.github.com>
2019-08-22 15:09:12 +0700

    Finished level screen
    - placeholder pause button asset added
    - finished level screen
    - finished Scene Transition component

--- Project Cruise/Assets/Scripts/Utilities/SceneTransition.cs
+++ Project Cruise/Assets/Scripts/Utilities/SceneTransition.cs
@@ -2,8 +2,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// A component to handle scene transitions generically.
+/// </summary>
 public class SceneTransition : MonoBehaviour
 {
+    // CLICK - use for UI buttons
+    // KEYDOWN - use for keys such as Escape
     public enum TransitionType { Click, KeyDown };
 
     public TransitionType transitionType = TransitionType.Click;
@@ -19,11 +24,19 @@ public class SceneTransition : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Loads a specific scene by name.
+    /// </summary>
+    /// <param name="sceneName">The name of the scene to be loaded.</param>
     public void LoadSceneByName(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
+    /// <summary>
+    /// Loads a scene when the specified key is pressed.
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator LoadSceneOnKeyDown()
     {
         while (!Input.GetKeyDown(keyToPress))
