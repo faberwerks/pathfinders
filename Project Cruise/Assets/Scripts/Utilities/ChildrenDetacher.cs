6776a2ee9ea5e99bc0038f8cab888f88a6268553
Phobez <36761666+Phobez@users.noreply.github.com>
2019-08-25 14:31:04 +0700

    Implemented level scene creator
    - Created Level Setup prefab
    - Created Children Detacher component to detach children and destroy itself on play
    - Reorganised folders

---
+++ Project Cruise/Assets/Scripts/Utilities/ChildrenDetacher.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A component to detach an object's children and destroys itself.
+/// </summary>
+public class ChildrenDetacher : MonoBehaviour
+{
+    private void Awake()
+    {
+        transform.DetachChildren();
+        Destroy(gameObject);
+    }
+}
