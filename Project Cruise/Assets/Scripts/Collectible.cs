33650f33225a446f9ceb4b1659b2e150f27e362c
Crouston <yosuam34@gmail.com>
2019-08-26 04:17:36 +0700

    Treasure implemented
    -Collectible and Treasure Script Added
    -Add TreasureDummy Prefab

---
+++ Project Cruise/Assets/Scripts/Collectible.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The Parent of all collectible item
+/// </summary>
+public class Collectible : MonoBehaviour
+{
+    //Called when Player touch the collectible
+    protected void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            OnCollected();
+            Destroy(gameObject);
+        }
+    }
+
+    //What the collected give when it touch by the Player
+    protected virtual void OnCollected()
+    {
+
+    }
+}
---
+++ Project Cruise/Assets/Scripts/Treasure.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treasure : Collectible
+{
+    //It add point to the treasure collected
+    protected override void OnCollected()
+    {
+        Blackboard.instance.LevelManager.TreasureCollected += 1;
+        Debug.Log(Blackboard.instance.LevelManager.TreasureCollected);
+    }
+}
