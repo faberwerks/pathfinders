01fdbde21827e4c2d2edc99e048f746a3b65f814
nalinai <44116980+nalinai@users.noreply.github.com>
2019-08-29 09:28:58 +0700

    Change Algorithm
    Samuel 29 August 2019  - Change Algorithm

--- Project Cruise/Assets/Scripts/Goal.cs
+++ Project Cruise/Assets/Scripts/Goal.cs
@@ -8,13 +8,15 @@ using UnityEngine;
 public class Goal : MonoBehaviour
 {
     /////// PROPERTIES ///////
-    public bool IsPressed { get; set; }
+    //Comented by Samuel 29 August 2019 - Change algoritm
+    //public bool IsPressed { get; set; }
 
     // Start is called before the first frame update
     void Start()
     {
         IsPressed = false;
         Blackboard.instance.LevelManager.goals.Add(this);
+        Blackboard.instance.LevelManager.TotalGoal +=1;
     }
 
     //// Update is called once per frame
@@ -26,18 +28,28 @@ public class Goal : MonoBehaviour
     //Method CheckGoals is called here so there is no need for this method called in update function 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
-        {
-            IsPressed = true;
-            Blackboard.instance.LevelManager.CheckGoals();
-        }
+    //Comented by Samuel 29 August 2019 - Change algoritm
+//         if(collision.CompareTag("Player"))
+//         {
+//             IsPressed = true;
+//             Blackboard.instance.LevelManager.CheckGoals();
+//         }
+         if(collision.CompareTag("Player"))
+         {
+            Blackboard.instance.LevelManager.Goal += 1;
+         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            IsPressed = false;
-        }
+    //Comented by Samuel 29 August 2019 - Change algoritm
+//         if (collision.CompareTag("Player"))
+//         {
+//             IsPressed = false;
+//         }
+         if(collision.CompareTag("Player"))
+         {
+            Blackboard.instance.LevelManager.Goal -= 1;
+         }
     }
 }
