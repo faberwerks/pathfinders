6e827e0eeee45f2d738cb403a35d342dd0ea60f6
Phobez <36761666+Phobez@users.noreply.github.com>
2019-08-30 23:00:06 +0700

    Updated Rule Tile Editor

--- Project Cruise/Assets/Scripts/Utilities/Editor/RuleTileEditor.cs
+++ Project Cruise/Assets/Scripts/Utilities/Editor/RuleTileEditor.cs
@@ -36,43 +36,9 @@ public class RuleTileEditor : Editor
         reorderableList.onReorderCallback = ListUpdated;
     }
 
-    public override void OnInspectorGUI()
+    private void ListUpdated(ReorderableList list)
     {
-        tile.defaultSprite = EditorGUILayout.ObjectField("Default Sprite", tile.defaultSprite, typeof(Sprite), false) as Sprite;
-        tile.defaultColliderType = (Tile.ColliderType)EditorGUILayout.EnumPopup("Default Collider", tile.defaultColliderType);
-        EditorGUILayout.Space();
-
-        if (reorderableList != null && tile.tilingRules != null)
-        {
-            reorderableList.DoLayoutList();
-        }
-    }
-
-    private void OnDrawHeader(Rect rect)
-    {
-        GUI.Label(rect, "Tiling Rules");
-    }
-
-    private void OnDrawElement(Rect rect, int index, bool isActive, bool isFocused)
-    {
-        RuleTile.TilingRule rule = tile.tilingRules[index];
-
-        float yPos = rect.yMin + 2.0f;
-        float height = rect.height - paddingBetweenRules;
-        float matrixWidth = defaultElementHeight;
-
-        Rect inspectorRect = new Rect(rect.xMin, yPos, rect.width - matrixWidth * 2.0f - 20.0f, height);
-        Rect matrixRect = new Rect(rect.xMax - matrixWidth * 2.0f - 10.0f, yPos, matrixWidth, defaultElementHeight);
-        Rect spriteRect = new Rect(rect.xMax - matrixWidth - 5.0f, yPos, matrixWidth, defaultElementHeight);
-
-        EditorGUI.BeginChangeCheck();
-        RuleInspectorOnGUI(inspectorRect, rule);
-        RuleMatrixOnGUI(matrixRect, rule);
-        SpriteOnGUI(spriteRect, rule);
-        if (EditorGUI.EndChangeCheck())
-        {
-            SaveTile();
-        }
+        SaveTile();
     }
 
     private float GetElementHeight(int index)
@@ -88,9 +54,28 @@ public class RuleTileEditor : Editor
         return defaultElementHeight + paddingBetweenRules;
     }
 
-    private void ListUpdated(ReorderableList list)
+    private void OnDrawElement(Rect rect, int index, bool isActive, bool isFocused)
     {
-        SaveTile();
+        RuleTile.TilingRule rule = tile.tilingRules[index];
+
+        Debug.Log("OnDrawElement yMin:" + rect.yMin);
+        float yPos = rect.yMin + 2.0f;
+        float height = rect.height - paddingBetweenRules;
+        float matrixWidth = defaultElementHeight;
+
+        Debug.Log("OnDrawElement yPos:" + yPos);
+        Rect inspectorRect = new Rect(rect.xMin, yPos, rect.width - matrixWidth * 2.0f - 20.0f, height);
+        Rect matrixRect = new Rect(rect.xMax - matrixWidth * 2.0f - 10.0f, yPos, matrixWidth, defaultElementHeight);
+        Rect spriteRect = new Rect(rect.xMax - matrixWidth - 5.0f, yPos, matrixWidth, defaultElementHeight);
+
+        EditorGUI.BeginChangeCheck();
+        RuleInspectorOnGUI(inspectorRect, rule);
+        RuleMatrixOnGUI(matrixRect, rule);
+        SpriteOnGUI(spriteRect, rule);
+        if (EditorGUI.EndChangeCheck())
+        {
+            SaveTile();
+        }
     }
 
     private void SaveTile()
@@ -99,8 +84,33 @@ public class RuleTileEditor : Editor
         SceneView.RepaintAll();
     }
 
+
+
+    private void OnDrawHeader(Rect rect)
+    {
+        GUI.Label(rect, "Tiling Rules");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        tile.defaultSprite = EditorGUILayout.ObjectField("Default Sprite", tile.defaultSprite, typeof(Sprite), false) as Sprite;
+        tile.defaultColliderType = (Tile.ColliderType)EditorGUILayout.EnumPopup("Default Collider", tile.defaultColliderType);
+        EditorGUILayout.Space();
+
+        if (reorderableList != null && tile.tilingRules != null)
+        {
+            reorderableList.DoLayoutList();
+        }
+    }
+
+
+
+
+
+
     private static void RuleInspectorOnGUI(Rect rect, RuleTile.TilingRule tilingRule)
     {
+        Debug.Log("Current yMin:" + rect.yMin);
         float y = rect.yMin;
         EditorGUI.BeginChangeCheck();
         GUI.Label(new Rect(rect.xMin, y, labelWidth, singleLineHeight), "Collider");
@@ -109,6 +119,14 @@ public class RuleTileEditor : Editor
         GUI.Label(new Rect(rect.xMin, y, labelWidth, singleLineHeight), "Output");
         tilingRule.output = (RuleTile.TilingRule.OutputSprite)EditorGUI.EnumPopup(new Rect(rect.xMin + labelWidth, y, rect.width - labelWidth, singleLineHeight), tilingRule.output);
         y += singleLineHeight;
+
+        Debug.Log("Current y:" + y);
+        if (tilingRule.output == RuleTile.TilingRule.OutputSprite.Random)
+        {
+            GUI.Label(new Rect(rect.xMin, y, labelWidth, singleLineHeight), "Noise");
+            tilingRule.perlinScale = EditorGUI.Slider(new Rect(rect.xMin + labelWidth, y, rect.width - labelWidth, singleLineHeight), tilingRule.perlinScale, 0.001f, 0.999f);
+            y += singleLineHeight;
+        }
 
         if (tilingRule.output == RuleTile.TilingRule.OutputSprite.Single)
         {
@@ -127,7 +145,70 @@ public class RuleTileEditor : Editor
             {
                 tilingRule.sprites[i] = EditorGUI.ObjectField(new Rect(rect.xMin + labelWidth, y, rect.width - labelWidth, singleLineHeight), tilingRule.sprites[i], typeof(Sprite), false) as Sprite;
                 y += singleLineHeight;
+                y += singleLineHeight;
+            }
+
+            Debug.Log("Current y at end:" + y);
+        }
+
+    }
+
+    private static void RuleMatrixOnGUI(Rect rect, RuleTile.TilingRule tilingRule)
+    {
+        Handles.color = new Color(0.0f, 0.0f, 0.0f, 0.2f);
+        int index = 0;
+        float w = rect.width / 3.0f;
+        float h = rect.height / 3.0f;
+
+        for (int y = 0; y <= 3; y++)
+        {
+            float top = rect.yMin + y * h;
+            Handles.DrawLine(new Vector3(rect.xMin, top), new Vector3(rect.xMax, top));
+        }
+        for (int x = 0; x <= 3; x++)
+        {
+            float left = rect.xMin + x * w;
+            Handles.DrawLine(new Vector3(left, rect.yMin), new Vector3(left, rect.yMax));
+        }
+
+        Handles.color = Color.white;
+
+        for (int y = 0; y <= 2; y++)
+        {
+            for (int x = 0; x <= 2; x++)
+            {
+                Rect r = new Rect(rect.xMin + x * w, rect.yMin + y * h, w - 1, h - 1);
+                if (x != 1 || y != 1)
+                {
+                    switch (tilingRule.neighbours[index])
+                    {
+                        case RuleTile.TilingRule.Neighbour.This:
+                            // draw arrow texture
+                            break;
+                        case RuleTile.TilingRule.Neighbour.NotThis:
+                            // draw x texture
+                            break;
+                    }
+
+                    if (Event.current.type == EventType.MouseDown && r.Contains(Event.current.mousePosition))
+                    {
+                        tilingRule.neighbours[index] = (RuleTile.TilingRule.Neighbour)(((int)tilingRule.neighbours[index] + 1) % 3);
+                        GUI.changed = true;
+                        Event.current.Use();
+                    }
+
+                    index++;
+                }
+                else
+                {
+                    // draw transform rule texture
+                }
             }
         }
+    }
+
+    private void SpriteOnGUI(Rect rect, RuleTile.TilingRule tilingRule)
+    {
+        tilingRule.sprites[0] = EditorGUI.ObjectField(new Rect(rect.xMax - rect.height, rect.yMin, rect.height, rect.height), tilingRule.sprites[0], typeof(Sprite), false) as Sprite;
     }
 }
