5c85692154981bd712f25720440b14046a69c360
Adhiwira Lokacarya <adhiwira56@gmail.com>
2019-08-30 19:28:40 +0700

    Pressure Plates Implement
    -Toogler Script for Pressure Plate added.
    - Pressure Plate prefab "black square" added

---
+++ Project Cruise/Assets/Scripts/Toggler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// A script to manage toogleObjects
+/// Do not forget to determine size Object that need to be toogled and put GameObjects in inspector
+/// </summary>
+public class Toggler : MonoBehaviour
+{
+    public enum TogglerType
+    {
+        NORMAL
+    }
+
+    public List<GameObject> toggledObjects;
+
+    public TogglerType togglerType = TogglerType.NORMAL;
+
+   // private bool hasBeenToggled;
+
+    private void Start()
+    {
+        //hasBeenToggled = false;
+    }
+
+    public void ToggleObjects()
+    {
+        Debug.Log("Lever: ToggleObjects called.");
+        for (int i = 0; i < toggledObjects.Count; i++)
+        {
+            toggledObjects[i].SetActive(!toggledObjects[i].activeInHierarchy);
+        }
+    }
+}
