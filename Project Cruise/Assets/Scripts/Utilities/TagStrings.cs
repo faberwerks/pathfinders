97d2a703122159ad74c262092b26be53d847e85a
Phobez <36761666+Phobez@users.noreply.github.com>
2019-08-31 21:25:50 +0700

    Created TagStrings class

---
+++ Project Cruise/Assets/Scripts/Utilities/TagStrings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditorInternal;
+
+public class TagStrings
+{
+    private static TagStrings instance = null;
+
+    public static TagStrings Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new TagStrings();
+            }
+            return instance;
+        }
+    }
+
+    private TagStrings()
+    {
+        
+    }
+}
