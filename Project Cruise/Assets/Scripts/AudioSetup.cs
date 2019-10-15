cf47013d2f9e570420f9b6f93f3a301a3190f025
Phobez <abiaph@live.com>
2019-10-15 16:30:42 +0700

    Implemented sound settings
    Co-Authored-By: zalfornt <zalfornt@users.noreply.github.com>

---
+++ Project Cruise/Assets/Scripts/AudioSetup.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+/// <summary>
+/// A component to set up initial audio settings.
+/// </summary>
+public class AudioSetup : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        SetUpAudioGroup("BGMVol");
+        SetUpAudioGroup("SFXVol");
+    }
+
+    /// <summary>
+    /// A method to check if Audio Group exists and set the mixer value accordingly.
+    /// </summary>
+    /// <param name="group"></param>
+    private void SetUpAudioGroup(string group)
+    {
+        if (!PlayerPrefs.HasKey(group))
+        {
+            PlayerPrefs.SetInt(group, 1);
+        }
+
+        if (PlayerPrefs.GetInt(group) == 0)
+        {
+            mixer.SetFloat(group, -80.0f);
+        }
+        else
+        {
+            mixer.SetFloat(group, 0.0f);
+        }
+    }
+}
---
+++ Project Cruise/Assets/Scripts/SoundController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+/// <summary>
+/// A component to handle sound setting.
+/// </summary>
+public class SoundController : MonoBehaviour
+{
+    public AudioMixer mixer;
+    private float volume;
+    private bool BGMOn;
+    private bool SFXOn;
+
+    private void Start()
+    {
+        BGMOn = PlayerPrefs.GetInt("BGMVol") == 1 ? true : false;
+        SFXOn = PlayerPrefs.GetInt("SFXVol") == 1 ? true : false;
+        //SET THE ICON ASSET
+    }
+
+    /// <summary>
+    /// Toggles an AudioGroup on and off.
+    /// </summary>
+    /// <param name="group"></param>
+    public void ToggleSound(string group)
+    {
+        if (PlayerPrefs.GetInt(group) == 1)
+        {
+            mixer.SetFloat(group, -80.0f);
+            PlayerPrefs.SetInt(group, 0);
+        }
+        else
+        {
+            mixer.SetFloat(group, 0.0f);
+            PlayerPrefs.SetInt(group, 1);
+        }
+    }
+}
