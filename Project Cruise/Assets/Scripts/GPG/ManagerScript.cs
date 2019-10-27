9f1e9017b05b37866f7e533ee5dc670c31052230
Phobez <36761666+Phobez@users.noreply.github.com>
2019-10-27 19:51:08 +0700

    Set up GPGS, achievements, and leaderboards

---
+++ Project Cruise/Assets/Scripts/GPG/ManagerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ManagerScript : MonoBehaviour
+{
+    public static ManagerScript Instance { get; private set; }
+    public static int Counter { get; private set; }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    public void IncrementCounter()
+    {
+        Counter++;
+        UIScript.Instance.UpdatePointsText();
+    }
+
+    public void RestartGame()
+    {
+        PlayGamesScript.AddScoreToLeaderboard(GPGSIds.leaderboard_test_leaderboard, Counter);
+        Counter = 0;
+        UIScript.Instance.UpdatePointsText();
+    }
+}
---
+++ Project Cruise/Assets/Scripts/GPG/PlayGamesScript.cs
@@ -0,0 +1,50 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+public class PlayGamesScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+
+        SignIn();
+    }
+
+    private void SignIn()
+    {
+        Social.localUser.Authenticate(success => { });
+    }
+
+    #region Achievements
+    public static void UnlockAchievement(string id)
+    {
+        Social.ReportProgress(id, 100, success => { });
+    }
+
+    public static void IncrementAchievement(string id, int stepsToIncrement)
+    {
+        PlayGamesPlatform.Instance.IncrementAchievement(id, stepsToIncrement, success => { });
+    }
+
+    public static void ShowAchievementsUI()
+    {
+        Social.ShowAchievementsUI();
+    }
+    #endregion /Achievements
+
+    #region Leaderboards
+    public static void AddScoreToLeaderboard(string leaderboardId, long score)
+    {
+        Social.ReportScore(score, leaderboardId, success => { });
+    }
+
+    public static void ShowLeaderboardsUI()
+    {
+        Social.ShowLeaderboardUI();
+    }
+    #endregion /Leaderboards
+}
