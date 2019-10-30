ff7bebdaef861131ddb0ee7bf4eb17b47f71be4c
Phobez <abiaph@live.com>
2019-10-30 20:07:22 +0700

    GPGS package and scripts imported

---
+++ Project Cruise/Assets/Scripts/GPG/ManagerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ManagerScript : MonoBehaviour
+{
+    public static ManagerScript Instance { get; private set; }
+    public static int Counter { get; private set; }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Instance = this;
+        UIScript.Instance.UpdateHighscoreText();
+    }
+
+    public void IncrementCounter()
+    {
+        Counter++;
+        UIScript.Instance.UpdatePointsText();
+    }
+
+    public void RestartGame()
+    {
+        //PlayGamesScript.AddScoreToLeaderboard(GPGSIds.leaderboard_test_leaderboard, Counter);
+
+        if (Counter > CloudVariables.Highscore)
+        {
+            CloudVariables.Highscore = Counter;
+            PlayGamesScript.Instance.SaveData();
+            UIScript.Instance.UpdateHighscoreText();
+        }
+
+        Counter = 0;
+        UIScript.Instance.UpdatePointsText();
+    }
+}
---
+++ Project Cruise/Assets/Scripts/GPG/UIScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class UIScript : MonoBehaviour
+{
+    public static UIScript Instance { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    [SerializeField]
+    private TMP_Text pointsTxt;
+    [SerializeField]
+    private TMP_Text highscoreTxt;
+
+    public void GetPoint()
+    {
+        ManagerScript.Instance.IncrementCounter();
+    }
+
+    public void Restart()
+    {
+        ManagerScript.Instance.RestartGame();
+    }
+
+    public void Increment()
+    {
+        PlayGamesScript.IncrementAchievement(GPGSIds.achievement_incremental_achievement, 5);
+    }
+
+    public void Unlock()
+    {
+        PlayGamesScript.UnlockAchievement(GPGSIds.achievement_working_achievement);
+    }
+
+    public void ShowAchievements()
+    {
+        PlayGamesScript.ShowAchievementsUI();
+    }
+
+    public void ShowLeaderboard()
+    {
+        PlayGamesScript.ShowLeaderboardsUI();
+    }
+
+    public void UpdatePointsText()
+    {
+        pointsTxt.text = ManagerScript.Counter.ToString();
+    }
+
+    public void UpdateHighscoreText()
+    {
+        highscoreTxt.text = CloudVariables.Highscore.ToString();
+    }
+}
