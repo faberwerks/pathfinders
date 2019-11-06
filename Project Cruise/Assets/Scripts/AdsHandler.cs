8f181174a1cb62af5e5ac054040c611c6ef8e578
Crouston <yosuam34@gmail.com>
2019-11-06 15:43:23 +0700

    Ads follow official admob

--- Project Cruise/Assets/Scripts/AdsHandler.cs
+++ Project Cruise/Assets/Scripts/AdsHandler.cs
@@ -3,121 +3,96 @@ using UnityEngine;
 using GoogleMobileAds.Api;
 
 // Example script showing how to invoke the Google Mobile Ads Unity plugin.
-public class AdsHandler : MonoBehaviour
+public class AdsHandler
 {
 
-    public InterstitialAd interstitialAd;
-    public RewardBasedVideoAd rewardedVideoAd;
-
-    private void Start()
+    private InterstitialAd interstitial;
+    private static AdsHandler instance;
+    public static AdsHandler Instance
     {
-        MobileAds.Initialize(initStatus => { });
-        RequestInterstitialAD();
-        DisplayInterstitialAD();
-    }
-
-    public void RequestInterstitialAD()
-    {
-        String interstitial_ID = "ca-app-pub-7623091422700152/2521661296";
-        interstitialAd = new InterstitialAd(interstitial_ID);
-
-        AdRequest adRequest = new AdRequest.Builder().Build();
-        interstitialAd.LoadAd(adRequest);
-        Debug.Log("Ads Loaded");
-    }
-
-    public void DisplayInterstitialAD()
-    {
-        if(interstitialAd.IsLoaded())
+        get
         {
-            Debug.Log("Ads Appear");
-            interstitialAd.Show();
+            if (instance == null)
+            {
+                instance = new AdsHandler();
+            }
+            return instance;
         }
     }
+    
 
-    public void RequestRewardedVideoAD()
-    {
-        String video_ID = "ca-app-pub-7623091422700152/3124296527";
-
-        AdRequest adRequest = new AdRequest.Builder().Build();
-        rewardedVideoAd.LoadAd(adRequest,video_ID);
-        Debug.Log("Ads Loaded");
-    }
-
-    public void DisplayRewardedVideoAD()
-    {
-        if (rewardedVideoAd.IsLoaded())
-        {
-            Debug.Log("Ads Appear");
-            rewardedVideoAd.Show();
-        }
-    }
-
-
-
-    //public void HandleInterstitialADEvent(bool subscribe)
+    //private void Awake()
     //{
-    //    if (subscribe)
+    //    if(instance == null)
     //    {
-    //        // Called when an ad request has successfully loaded.
-    //        interstitialAd.OnAdLoaded += HandleOnAdLoaded;
-    //        // Called when an ad request failed to load.
-    //        interstitialAd.OnAdFailedToLoad += HandleOnAdFailedToLoad;
-    //        // Called when an ad is shown.
-    //        interstitialAd.OnAdOpening += HandleOnAdOpened;
-    //        // Called when the ad is closed.
-    //        interstitialAd.OnAdClosed += HandleOnAdClosed;
-    //        // Called when the ad click caused the user to leave the application.
-    //        interstitialAd.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+    //        instance = this;
     //    }
     //    else
     //    {
-    //        // Called when an ad request has successfully loaded.
-    //        interstitialAd.OnAdLoaded -= HandleOnAdLoaded;
-    //        // Called when an ad request failed to load.
-    //        interstitialAd.OnAdFailedToLoad -= HandleOnAdFailedToLoad;
-    //        // Called when an ad is shown.
-    //        interstitialAd.OnAdOpening -= HandleOnAdOpened;
-    //        // Called when the ad is closed.
-    //        interstitialAd.OnAdClosed -= HandleOnAdClosed;
-    //        // Called when the ad click caused the user to leave the application.
-    //        interstitialAd.OnAdLeavingApplication -= HandleOnAdLeavingApplication;
+    //        Destroy(this);
     //    }
     //}
 
-
-    //public void HandleOnAdLoaded(object sender, EventArgs args)
+    //public void Start()
     //{
-    //    DisplayInterstitialAD();
+    //    MobileAds.Initialize(AppID);
     //}
 
-    //public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
-    //{
-    //    RequestInterstitialAD();
-    //}
-
-    //public void HandleOnAdOpened(object sender, EventArgs args)
-    //{
-    //    MonoBehaviour.print("HandleAdOpened event received");
-    //}
-
-    //public void HandleOnAdClosed(object sender, EventArgs args)
-    //{
-    //    MonoBehaviour.print("HandleAdClosed event received");
-    //}
-
-    //public void HandleOnAdLeavingApplication(object sender, EventArgs args)
-    //{
-    //    MonoBehaviour.print("HandleAdLeavingApplication event received");
-    //}
-
-    //public void OnEnable()
-    //{
-    //    HandleInterstitialADEvent(true);
-    //}
-
-    public void OnDisable()
+    public void RequestInterstitialAD()
     {
-        interstitialAd.Destroy();
+        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+        this.interstitial = new InterstitialAd(adUnitId);
+
+        // Called when an ad request has successfully loaded.
+        this.interstitial.OnAdLoaded += HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.interstitial.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+        // Called when an ad is shown.
+        this.interstitial.OnAdOpening += HandleOnAdOpened;
+        // Called when the ad is closed.
+        this.interstitial.OnAdClosed += HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        this.interstitial.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+        
+    }
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleOnAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+        this.interstitial.Destroy();
+    }
+
+    public void HandleOnAdLeavingApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeavingApplication event received");
+    }
+
+    public void ShowInterstitialAD()
+    {
+        if (this.interstitial.IsLoaded())
+        {
+            this.interstitial.Show();
+        }
     }
 }
---
+++ Project Cruise/Assets/Scripts/ShowAds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class ShowAds : MonoBehaviour
+{
+
+    private string AppID = "ca-app-pub-7623091422700152~9683332999";
+    // Start is called before the first frame update
+    void Start()
+    {
+        MobileAds.Initialize(AppID);
+        AdsHandler.Instance.RequestInterstitialAD();
+        AdsHandler.Instance.ShowInterstitialAD();
+    }
+    
+}
