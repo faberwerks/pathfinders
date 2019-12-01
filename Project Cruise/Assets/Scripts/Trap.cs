abedb06a8a6d9393aac74597edf0d2802149ff61
Crouston <yosuam34@gmail.com>
2019-12-02 03:13:06 +0700

    sfx added

--- Project Cruise/Assets/Scripts/Trap.cs
+++ Project Cruise/Assets/Scripts/Trap.cs
@@ -2,10 +2,14 @@
 
 public class Trap : MonoBehaviour
 {
+    public AudioSource hitOnTrap;
+    public AudioSource die;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag(TagStrings.PLAYER_TAG))
         {
+            hitOnTrap.Play();
+            die.Play();
             Blackboard.Instance.LevelManager.Lose();
         }
     }
