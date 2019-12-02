c4bdb471b313b0f6f050ecdb12960504027f349d
zalfornt <44518346+zalfornt@users.noreply.github.com>
2019-12-02 17:08:47 +0700

    Add coin data fetcher

---
+++ Project Cruise/Assets/Scripts/CoinDataFetcher.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class CoinDataFetcher : MonoBehaviour
+{
+    public TMP_Text coins;
+
+    void Start()
+    {
+        //this.coins = GameData.Instance.saveData.coins;
+    }
+}
