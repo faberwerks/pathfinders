f8e884b831963961bcd40af5208b4de5c3f4b61b
Phobez <36761666+Phobez@users.noreply.github.com>
2019-12-02 21:02:55 +0700

    Implemented pooled Audio Player system.
    - added Audio Player Manager, Audio Player, and Audio Hnadler components
    - modified Audio Setup to also set up Audio Player system
    
    Co-Authored-By: nalinai <nalinai@users.noreply.github.com>

---
+++ Project Cruise/Assets/Scripts/AudioHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Component to send a request to the Audio Player Manager to play an audio clip.
+/// </summary>
+public class AudioHandler : MonoBehaviour
+{
+    /// <summary>
+    /// Sends an audio clip to the Audio Player Manager to play.
+    /// </summary>
+    /// <param name="audioClip">Audio clip to be played.</param>
+    public void Play(AudioClip audioClip)
+    {
+        AudioPlayerManager.Instance.Play(audioClip);
+    }
+}
---
+++ Project Cruise/Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Component to play audio clips.
+/// </summary>
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioSource AudioSource { get; private set; }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        AudioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// Plays an audio clip.
+    /// </summary>
+    /// <param name="audioClip">Audio clip to be played.</param>
+    public void Play(AudioClip audioClip)
+    {
+        AudioSource.PlayOneShot(audioClip);
+    }
+}
