2e9a6b762a98a34bbe456bc5e571575a9f7544ee
Crouston <yosuam34@gmail.com>
2020-01-27 17:46:16 +0700

    lighting prototype

---
+++ Project Cruise/Assets/Scripts/Mechanics/PowerUp/SwitchLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchLight : MonoBehaviour
+{
+    public GameObject globalLightDim, globalLightBright;
+
+    public void TurnOffLight()
+    {
+        globalLightDim.SetActive(true);
+        globalLightBright.SetActive(false);
+    }
+
+    public void TurnOnLight()
+    {
+        globalLightDim.SetActive(false);
+        globalLightBright.SetActive(true);
+        Invoke("TurnOffLight", 3f);
+    }
+}
