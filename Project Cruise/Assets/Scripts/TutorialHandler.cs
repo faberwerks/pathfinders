6c8f8ac8ef79d9bfc132a75aaf1eda299b48e0e2
Crouston <yosuam34@gmail.com>
2020-02-14 05:24:00 +0700

    Implement tutorial for M6 scene
    added script and masking.

---
+++ Project Cruise/Assets/Scripts/TutorialHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialHandler : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+            Destroy(gameObject);
+    }
+}
