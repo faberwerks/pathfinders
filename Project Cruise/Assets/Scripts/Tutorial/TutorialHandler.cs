7f05d317dfad73c9a627841e9287bbe157d00ec5
zalfornt <44518346+zalfornt@users.noreply.github.com>
2020-02-14 18:04:52 +0800

    Added Time control
    Added TimeScale control in TutorialHandler.cs

--- Project Cruise/Assets/Scripts/Tutorial/TutorialHandler.cs
+++ Project Cruise/Assets/Scripts/Tutorial/TutorialHandler.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class TutorialHandler : MonoBehaviour
 {
     // Update is called once per frame
+
+    private void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
     void Update()
     {
         if (Input.anyKeyDown)
+        {
             Destroy(gameObject);
+            Time.timeScale = 1f;
+        }
+        
     }
 }
