347aeedfa5f8b5ba0180fb691780d8a1a1a3748a
Phobez <36761666+Phobez@users.noreply.github.com>
2020-02-18 10:38:43 +0700

    Implemented unlocking other stages.

---
+++ Project Cruise/Assets/StageLock.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StageLock : MonoBehaviour
+{
+    public int firstLevelNumber = 1;
+    public Sprite unlockedSprite = null;
+    public Sprite lockedSprite = null;
+
+    private Button button = null;
+    private Image img = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        img = GetComponent<Image>();
+
+        if (GameData.Instance.saveData.LastLevelNumber >= firstLevelNumber)
+        {
+            Unlock();
+        }
+        else
+        {
+            Lock();
+        }
+    }
+
+    private void Lock()
+    {
+        if (lockedSprite)
+        {
+            img.sprite = lockedSprite;
+        }
+        button.interactable = false;
+    }
+
+    private void Unlock()
+    {
+        if (unlockedSprite)
+        {
+            img.sprite = unlockedSprite;
+        }
+        button.interactable = true;
+    }
+}
