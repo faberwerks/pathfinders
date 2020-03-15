e2a9a2c46316aeb3dd20137ccb0d2bdaaa388ac7
Phobez <36761666+Phobez@users.noreply.github.com>
2020-03-15 15:46:53 +0700

    Added debugging tools.
    - Added input field and button to load levels for debug in Main Menu
    - Added Start button to Tools to start the game from Loading

---
+++ Project Cruise/Assets/Scripts/Utilities/DebugSceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class DebugSceneLoader : MonoBehaviour
+{
+    public GameObject debugUI = null;
+    public TMP_InputField input = null;
+
+    private void Start()
+    {
+        if (!Application.isEditor)
+        {
+            debugUI.SetActive(false);
+        }
+    }
+
+    public void DebugLoadScene()
+    {
+        SceneManager.LoadScene(input.text);
+    }
+
+    // add menu named "Create New Level" to "Tools" menu
+    [MenuItem("Tools/Start")]
+    public static void Init()
+    {
+        EditorSceneManager.OpenScene("Assets/Scenes/Loading.unity");
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+}
