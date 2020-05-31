480ab6965516b145d722c97349645f34325a82fb
Phobez <36761666+Phobez@users.noreply.github.com>
2020-05-31 16:15:50 +0700

    Implemented BGM audio system and added menu BGM.

---
+++ Project Cruise/Assets/Scripts/BGMAudioPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Component to play background music.
+/// </summary>
+public class BGMAudioPlayer : AudioPlayer
+{
+    [SerializeField]
+    private AudioClip mainMenuBGM = null;
+    [SerializeField]
+    private AudioClip inGameBGM = null;
+    [SerializeField]
+    private List<string> menuScenes;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        PlayBGM(mainMenuBGM);
+        SceneManager.activeSceneChanged += CheckSceneName;
+    }
+
+    /// <summary>
+    /// Checks the scene name and changes the BGM played if necessary.
+    /// </summary>
+    private void CheckSceneName(Scene current, Scene next)
+    {
+        if (menuScenes.Contains(next.name))
+        {
+            Debug.Log("[BGMAudioPlayer] Menu Scene: " + next.name);
+            if (AudioSource.clip != mainMenuBGM)
+            {
+                PlayBGM(mainMenuBGM);
+            }
+        }
+        else
+        {
+            Debug.Log("[BGMAudioPlayer] Not Menu Scene: " + next.name);
+            if (AudioSource.clip != inGameBGM)
+            {
+                PlayBGM(inGameBGM);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Play a BGM.
+    /// </summary>
+    /// <param name="bgm">BGM to be played.</param>
+    private void PlayBGM(AudioClip bgm)
+    {
+        AudioSource.clip = bgm;
+        AudioSource.Play();
+    }
+}
