aee3325651d1b16f248d9c9801e33fb5920f9334
nalinai <samueledselfernandez@gmail.com>
2020-06-16 20:00:34 +0700

    double lose and time still run when character is on dying animation bug fixed
    Co-Author : @phobez
    show lose menu twice when character die twice bug fixed , and time still running when character on death animation bug fixed

--- Project Cruise/Assets/Scripts/Mechanics/Trap.cs
+++ Project Cruise/Assets/Scripts/Mechanics/Trap.cs
@@ -6,18 +6,38 @@ public class Trap : MonoBehaviour
 
     private AudioHandler audioHandler;
 
+    public static int totalCollide;
+
     private void Start()
     {
         audioHandler = GetComponent<AudioHandler>();
+        ResetTotalCollide();
+    }
+
+    private void ResetTotalCollide()
+    {
+        Debug.Log("reset");
+        totalCollide = 0;
+    }
+
+    void Update()
+    {
+        if (totalCollide > 0)
+        {
+            ResetTotalCollide();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag(TagStrings.PLAYER_TAG))
+        if (collision.gameObject.CompareTag(TagStrings.PLAYER_TAG) && totalCollide < 1)
         {
+            Debug.Log("Die");
             audioHandler.Play(hitSound);
+            totalCollide += 1;
             collision.gameObject.GetComponent<Animator>().SetTrigger("Die");
             Blackboard.Instance.LevelManager.DisableCharacterMovement();
+            Blackboard.Instance.LevelManager.EndLevelTimer();
         }
     }
 }
