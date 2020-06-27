c147f066d996f2e36dc5eace672979919c795cc7
Phobez <36761666+Phobez@users.noreply.github.com>
2020-06-27 10:01:13 +0700

    Added level label to the pause menu.

---
+++ Project Cruise/Assets/Scripts/UI/LevelLabelHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class LevelLabelHandler : MonoBehaviour
+{
+    private TMP_Text levelLabel;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        levelLabel = GetComponent<TMP_Text>();
+
+        levelLabel.text = "Level " + GameData.Instance.currLevelID;
+    }
+}
