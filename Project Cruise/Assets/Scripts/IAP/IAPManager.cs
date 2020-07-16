26ebad8ea3cda5ea2777d5b3b7c2700a3e9f3362
Phobez <36761666+Phobez@users.noreply.github.com>
2020-07-16 21:08:50 +0700

    IAP removed.
    MInimum API level set to 17 because of error with AndroidManifest.xml.

--- Project Cruise/Assets/Scripts/IAP/IAPManager.cs
+++ Project Cruise/Assets/Scripts/IAP/IAPManager.cs
@@ -1,20 +1,20 @@
-﻿using UnityEngine;
-using UnityEngine.Purchasing;
+﻿//using UnityEngine;
+//using UnityEngine.Purchasing;
 
-public class IAPManager : MonoBehaviour
-{
-    /// <summary>
-    /// Directly adds Coins the quantity in the payout from the corresponding product.
-    /// </summary>
-    /// <param name="bought">The product that will be taken from the IAP Button component</param>
-    public void AddCoinPurchase(Product bought)
-    {
-        GameData.Instance.saveData.Coins += (int)bought.definition.payout.quantity;
-        Debug.Log("[IAP] Product ID: " + bought.definition.id);
-    }
+//public class IAPManager : MonoBehaviour
+//{
+//    /// <summary>
+//    /// Directly adds Coins the quantity in the payout from the corresponding product.
+//    /// </summary>
+//    /// <param name="bought">The product that will be taken from the IAP Button component</param>
+//    public void AddCoinPurchase(Product bought)
+//    {
+//        GameData.Instance.saveData.Coins += (int)bought.definition.payout.quantity;
+//        Debug.Log("[IAP] Product ID: " + bought.definition.id);
+//    }
 
-    public void SetNoAds(Product bought)
-    {
-        //TO-DO: set trigger to stop ads
-    }
-}
+//    public void SetNoAds(Product bought)
+//    {
+//        //TO-DO: set trigger to stop ads
+//    }
+//}
