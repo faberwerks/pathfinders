aae9faca4e15946f3c130e0704c03966b64c9113
Phobez <36761666+Phobez@users.noreply.github.com>
2020-07-17 22:03:13 +0700

    Upgraded to 2019.3.15f1 and made new scene files for all levels.
    Unity failed to upgrade the files properly -> no tilemap collider.

--- Project Cruise/Assets/Scripts/Utilities/Editor/DebugStarter.cs
+++ Project Cruise/Assets/Scripts/Utilities/Editor/DebugStarter.cs
@@ -1,5 +1,7 @@
-﻿using UnityEditor;
+﻿using UnityEngine;
+using UnityEditor;
 using UnityEditor.SceneManagement;
+using System.Collections.Generic;
 
 public class DebugStarter
 {
@@ -18,5 +20,22 @@ public class DebugStarter
 
         EditorSceneManager.OpenScene("Assets/Scenes/Loading.unity");
         EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+
+    [MenuItem("Tools/List Scene Names")]
+    public static void LogNames()
+    {
+        List<string> temp = new List<string>();
+        EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
+        for (int i = 7; i < 27; i++)
+        {
+                string name = scenes[i].path.Substring(scenes[i].path.LastIndexOf('/') + 1);
+                name = name.Substring(0, name.Length - 6);
+
+                EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+
+                string path = string.Format("Assets/Scenes/Levels/NEW/{0}.unity", name);
+                EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene(), path);
+        }
     }
 }
---
+++ Project Cruise/Assets/Scripts/Utilities/RemakeSceneFiles.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ReadSceneNames : MonoBehaviour
+{
+    public string[] scenes;
+#if UNITY_EDITOR
+    private static string[] ReadNames()
+    {
+        List<string> temp = new List<string>();
+        foreach (UnityEditor.EditorBuildSettingsScene S in UnityEditor.EditorBuildSettings.scenes)
+        {
+            if (S.enabled)
+            {
+                string name = S.path.Substring(S.path.LastIndexOf('/') + 1);
+                name = name.Substring(0, name.Length - 6);
+                temp.Add(name);
+            }
+        }
+        return temp.ToArray();
+    }
+    [UnityEditor.MenuItem("CONTEXT/ReadSceneNames/Update Scene Names")]
+    private static void UpdateNames(UnityEditor.MenuCommand command)
+    {
+        ReadSceneNames context = (ReadSceneNames)command.context;
+        context.scenes = ReadNames();
+    }
+
+    [UnityEditor.MenuItem("CONTEXT/List Scene Names")]
+    private static void LogNames()
+    {
+        string[] scenes = ReadNames();
+
+        foreach (string scene in scenes)
+        {
+            Debug.Log(scene);
+        }
+    }
+
+    private void Reset()
+    {
+        scenes = ReadNames();
+    }
+#endif
+}
