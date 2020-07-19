0886b4063f32dce3e2bf6853ab90a53040f57605
Crouston <yosuam34@gmail.com>
2020-07-19 20:22:56 +0700

    Back handler updated
    -added some ifs to back handler script
    -added backhandler to level setup prefab

--- Project Cruise/Assets/Scripts/Scenes/BackHandler.cs
+++ Project Cruise/Assets/Scripts/Scenes/BackHandler.cs
@@ -6,19 +6,26 @@ using UnityEngine.SceneManagement;
 public class BackHandler : MonoBehaviour
 {
     // Update is called once per frame
+    public GameObject QuitCanvas;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().buildIndex == 1)
         {
-            Application.Quit();
+            //Application.Quit();
+            QuitCanvas.SetActive(true);
         }
-        else if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().buildIndex == 5)
+        else if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().buildIndex == 4)
         {
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(3);
         }
         else if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().buildIndex < 4 && SceneManager.GetActiveScene().buildIndex != 0)
         {
-            SceneManager.LoadScene(GameData.Instance.previousSceneID);
+            SceneManager.LoadScene(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().buildIndex > 4)
+        {
+            Blackboard.Instance.LevelManager.Pause(true);
+            Blackboard.Instance.LevelManager.pauseCanvas.SetActive(true);
         }
     }
 }
