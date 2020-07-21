265794f9bc386c3d9d9fd3a2fa77ea6d768d1bfd
zalfornt <44518346+zalfornt@users.noreply.github.com>
2020-07-21 20:26:21 +0700

    Added Controller setup

---
+++ Project Cruise/Assets/Scripts/ControllerTypeHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ControllerTypeHandler : MonoBehaviour
+{
+    public GameObject[] diagonalArrows;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("ClassicController", 0) == 1 ? true : false)
+        {
+            foreach (var arrow in diagonalArrows)
+            {
+                arrow.SetActive(false);
+            }
+        }
+    }
+}
